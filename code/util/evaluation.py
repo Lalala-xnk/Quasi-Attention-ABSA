9e6adb19c839ed350b75544edfd5e218bebbb9a9
Lalala-xnk <3170102078@zju.edu.cn>
2022-02-27 19:13:02 +0800

    added sentiment f1 score for semeval 2014

--- code/util/evaluation.py
+++ code/util/evaluation.py
@@ -168,6 +168,7 @@ def semeval_Acc(y_true, y_pred, score, classes=4):
         total=0
         total_right=0
         y_pred_classified = []
+        y_true_classified = []
         for i in range(len(y_true)):
             if y_true[i]==4:continue
             total+=1
@@ -184,8 +185,9 @@ def semeval_Acc(y_true, y_pred, score, classes=4):
             if y_true[i]==tmp:
                 total_right+=1
             y_pred_classified.append(tmp)
+            y_true_classified.append(y_true[i])
         sentiment_Acc = total_right/total
-        sentiment_f1 = metrics.f1_score(y_true, y_pred_classified, average='micro')
+        sentiment_f1 = metrics.f1_score(y_true_classified, y_pred_classified, average='micro')
     elif classes == 3:
         total=0
         total_right=0
